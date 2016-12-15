5e199b14ad511bd69b209e95cd47310b9fea3322
joni <jonathan.hyams@students.bfh.ch>
2016-12-15 11:37:32 +0100

    fixed #2 missing headers

--- src/test/java/ch/bfh/ti/soed/hs16/srs/white/model/ModelsTest.java
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/white/model/ModelsTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017 Berner Fachhochschule, Switzerland.
+ *
+ * Project Smart Reservation System.
+ *
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
 package ch.bfh.ti.soed.hs16.srs.white.model;
 
 import ch.bfh.ti.soed.hs16.srs.white.concept.Model;
